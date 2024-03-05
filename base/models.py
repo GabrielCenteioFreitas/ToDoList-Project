6096e7c905e07988d7dae00e19714445ab445208
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-03-04 22:31:32 -0300

    translate the application to portuguese

--- base/models.py
+++ base/models.py
@@ -4,14 +4,14 @@ from django.contrib.auth.models import User
 class Task(models.Model):
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, null=True, blank=True)
-    title = models.CharField(max_length=200)
-    description = models.TextField(null=True, blank=True)
-    complete = models.BooleanField(default=False)
+    titulo = models.CharField(max_length=200)
+    descricao = models.TextField(null=True, blank=True)
+    feita = models.BooleanField(default=False)
     create = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.title
+        return self.titulo
 
     class Meta:
-        ordering = ['complete']
+        ordering = ['feita']
     
