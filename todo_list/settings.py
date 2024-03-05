61dea23723d217550b3ae4bb48e1c5a75f358ded
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-03-05 01:02:56 -0300

    configure the environment to deploy

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -126,3 +126,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates/static')]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://web-production-d3de.up.railway.app']
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
