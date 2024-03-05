9268cea70366d17ee8a29114af1419b336985cce
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-03-05 10:48:39 -0300

    configure the environment to deploy

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -12,16 +12,17 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(md-wv1eq8l3e(y(39t7(d_(e+^#+0=0-^md0%b60#*+tvq%-s'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
